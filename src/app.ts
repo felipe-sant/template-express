0e2075273aed9628d3fdc310e7c815e1efd0c594
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-07-11 22:00:10 -0300

    feat: adicionando logger de requisicoes

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import exampleRoutes from './routes/Example.routes'
 import path from 'path'
-import i18next from "./config/i18n.config"
+import i18next from "./configs/i18n.config"
 import i18nextMiddleware from "i18next-http-middleware"
+import requestLogger from './middlewares/requestLogger.middleware'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
 app.use(i18nextMiddleware.handle(i18next))
 
 app.get('/', (_, res) => { res.redirect('/docs/routes.html')})
