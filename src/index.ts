262fffec765c78790d897cf22aba216c641019fb
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-08-20 23:19:11 +0000

    feat: adicionando portabilidade para diversas portas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Server rodando em http://localhost:${port}`);
+const port = 3001;
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server rodando em http://0.0.0.0:${port}`);
 });
