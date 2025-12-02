6d5007c43cb9f346331d4d2ced819f10759f742f
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2025-12-02 13:09:39 +0000

    #1 feat: adicionando express

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/", (_: Request, res: Response) => res.sendStatus(404))
+
+export default app
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+import app from "./app"
+
+const port = 3001
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`✅ | Servidor rodando na porta http://0.0.0.0:${port}`)
+})
